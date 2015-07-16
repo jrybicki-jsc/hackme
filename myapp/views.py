746623c434a0cd6e6d9fea7110a086023ae78f0d
httpPrincess <j.rybicki@fz-juelich.de>
2015-07-16 13:46:23 +0200

    csrf pof added

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,25 @@
 from myapp import app, login_manager
 from flask import request, render_template, redirect, url_for, flash
-from flask.ext.login import LoginManager, login_required, login_user
+from flask.ext.login import login_required, login_user, logout_user
+from datetime import datetime
+
+todos = list()
 
 
-@app.route('/', methods=['GET'])
+@app.before_first_request
+def init_todos():
+    global todos
+    todos.append({'date': datetime.now().strftime('%x'),
+                  'content': 'important news'})
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    global todos
+    if request.method == 'POST':
+        todos.append({'date': datetime.now().strftime('%x'),
+                      'content': request.form['content']})
+    return render_template('index.html', todos=todos)
 
 
 @app.route('/secret/')
@@ -17,7 +31,6 @@ def secret():
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # do the login
         next = request.args.get('next')
         name = request.form['username']
         password = request.form['password']
@@ -29,6 +42,13 @@ def login():
             flash('Wrong login')
 
     return render_template('login.html')
+
+
+@app.route('/logout/')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @login_manager.user_loader
